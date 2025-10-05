c8ba4e2511e34fe376e0485ddce051a9ced6ed57
Jaqueline Reis <jaquelinereizs@gmail.com>
2025-10-05 17:36:23 -0300

    fit: carregamento dinâmico dos boxes da tela boxes
    fit: carregamento dinâmico dos boxes da tela boxes

---
+++ src/services/getBoxes.ts
@@ -0,0 +1,25 @@
+import { supabase } from '../lib/supabaseClient'
+
+export interface Box {
+    id: number
+    deadline_date: number
+    box_title: string
+    box_description: string
+    box_area: number
+
+}
+
+export default async function getBoxes(): Promise<Box[]> {
+  
+    const { data, error } = await supabase
+      .from('BOX')
+      .select('id, deadline_date, box_title, box_description, box_area')
+
+    if (error) {
+      console.error("Erro ao buscar dados:", error)
+    } else {
+      console.log("Dados recebidos:", data)
+    }
+
+  return data as Box[]
+}
