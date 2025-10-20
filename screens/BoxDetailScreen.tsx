7c808ef4c35921f55d4a36fe816dbc6a6384054a
Jaqueline Reis <jaquelinereizs@gmail.com>
2025-10-19 21:55:24 -0300

    feature: permitir edição das informações do box
    feature: permitir edição das informações do box

--- screens/BoxDetailScreen.tsx
+++ screens/BoxDetailScreen.tsx
@@ -1,11 +1,12 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack"; 
 import { RootStackParamList } from "../src/navigation/types";
-import { Box, Item } from "../src/types";
+import { Item } from "../src/types";
 import { useEffect, useState } from "react";
-import { View, Text, FlatList, StyleSheet, SafeAreaView, TouchableOpacity } from "react-native";
+import { View, Text, FlatList, StyleSheet, SafeAreaView, TouchableOpacity, Modal } from "react-native";
 import getItems from "../src/services/getItems";
 import ItemCard from "../components/ItemCard";
 import { Ionicons } from "@expo/vector-icons";
+import BoxEditModal from "../components/BoxEditModal";
 
 type Props = NativeStackScreenProps<RootStackParamList, "BoxDetailScreen">;
 
@@ -13,6 +14,9 @@ export default function BoxDetailScreen({ route, navigation }: Props) {
   const { box } = route.params;
   const [items, setItems] = useState<Item[]>([]);
   const [fdata, setFData] = useState<string>();
+  const [optionsVisible, setOptionsVisible] = useState(false);
+  const [editModalVisible, setEditModalVisible] = useState(false);
+  const [currentBox, setCurrentBox] = useState(box);
 
   useEffect(() => {
     const fetchItems = async () => {
@@ -43,6 +47,15 @@ export default function BoxDetailScreen({ route, navigation }: Props) {
     }
   }, [box.id]);
 
+  useEffect(() => {
+    if (currentBox.deadline_date) {
+      const rData = new Date(currentBox.deadline_date);
+      setFData(rData.toLocaleDateString("pt-BR"));
+    } else {
+      setFData("Data não informada :)");
+    }
+  }, [currentBox]);
+
   return (
     <SafeAreaView style={styles.container}>
       {/* Header */}
@@ -51,16 +64,81 @@ export default function BoxDetailScreen({ route, navigation }: Props) {
           <Ionicons name="arrow-back" size={28} color="#eef4ed"/>
         </TouchableOpacity>
 
-        <Text style={styles.title}>{box.box_title}</Text>
+        <TouchableOpacity style={styles.optionsButton} onPress={() => setOptionsVisible(true)}>
+          <Ionicons name="ellipsis-vertical" size={28} color="#eef4ed" />
+        </TouchableOpacity>
+        
+        {/* Modal de opções */}
+        <Modal
+          visible={optionsVisible}
+          transparent
+          animationType="fade"
+          onRequestClose={() => setOptionsVisible(false)}
+        >
+        <View style={styles.overlay}>
+          <View style={styles.optionsContainer}>
+            {/* Header do Modal */}
+            <View style={styles.modalHeader}>
+              <Text style={styles.modalTitle}>Escolha uma opção</Text>
+              <TouchableOpacity
+                style={styles.closeButton}
+                onPress={() => setOptionsVisible(false)}
+              >
+                <Ionicons name="close" size={25} color="#4d535aff" />
+              </TouchableOpacity>
+            </View>
+              
+            {/* Botão Editar */}
+            <TouchableOpacity
+              style={styles.optionButton}
+              onPress={() => {
+                setOptionsVisible(false);
+                setEditModalVisible(true);
+              }}
+            >
+              <Ionicons name="create-outline" size={22} color="#034078" />
+              <Text style={styles.optionText}>Editar Box</Text>
+            </TouchableOpacity>
+
+            {/* Botão Excluir */}
+            <TouchableOpacity
+              style={[styles.optionButton, { borderTopWidth: 1, borderTopColor: "#eee" }]}
+              onPress={() => {
+                setOptionsVisible(false);
+                  // aqui será criado a lógica de excluir o box
+                  console.log("Excluir Box");
+                }}
+              >
+                <Ionicons name="trash-outline" size={22} color="#c1121f" />
+                <Text style={[styles.optionText, { color: "#c1121f" }]}>
+                  Excluir Box
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </Modal>
+
+        {/* Modal de edição */}
+        <BoxEditModal
+          visible={editModalVisible} onClose={() => setEditModalVisible(false)}
+          box={currentBox} onSave={(updatedBox) => {
+            setCurrentBox ({
+              ...currentBox,
+              ...updatedBox
+            });
+          }}
+        />
+
+        <Text style={styles.title}>{currentBox.box_title}</Text>
 
         {box.box_description ? (
-          <Text style={styles.description}>{box.box_description}</Text>
+          <Text style={styles.description}>{currentBox.box_description}</Text>
         ) : null}
 
         <View style={styles.infoContainer}>
           {box.area_name && (
             <Text style={styles.infoText}>
-              <Ionicons name="reader" size={16} color="#eef4ed"/> Área: {box.area_name}
+              <Ionicons name="reader" size={16} color="#eef4ed"/> Área: {currentBox.area_name}
             </Text>
           )}
           {fdata && (
@@ -111,6 +189,15 @@ const styles = StyleSheet.create({
     marginTop: 25,
     marginBottom: 25
   },
+  optionsButton: {
+    position: "absolute",
+    left: 360,
+    top: 25,
+    borderRadius: 8,
+    padding: 5,
+    marginTop: 25,
+    marginBottom: 25
+  },
   title: {
     fontSize: 24,
     fontWeight: "bold",
@@ -141,5 +228,61 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
     paddingTop: 20,
     paddingBottom: 40,
+  },
+  // ======= Menu de opções (box) =======
+  overlay: {
+    flex: 1,
+    backgroundColor: "rgba(0,0,0,0.4)",
+    justifyContent: "center",
+    alignItems: "center",
+    paddingHorizontal: 20,
+  },
+  optionsContainer: {
+    width: "85%",
+    backgroundColor: "#fff",
+    borderRadius: 16,
+    paddingVertical: 20,
+    paddingHorizontal: 20,
+    shadowColor: "#000",
+    shadowOpacity: 0.25,
+    shadowOffset: { width: 0, height: 4 },
+    shadowRadius: 6,
+    elevation: 6,
+  },
+  modalHeader: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 20,
+  },
+  modalTitle: {
+    fontSize: 20,
+    fontWeight: "700",
+    color: "#034078",
+    textAlign: "left",
+    flex: 1,
+  },
+  closeButton: {
+    position: "absolute",
+    right: 0,
+    top: 0,
+    padding: 2,
+  },
+  optionButton: {
+    flexDirection: "row",
+    alignItems: "center",
+    paddingVertical: 16,
+    borderBottomWidth: 1,
+    borderBottomColor: "#eee",
+  },
+  optionDelete: {
+    borderBottomWidth: 0,
+    marginTop: 4,
+  },
+  optionText: {
+    fontSize: 16,
+    color: "#034078",
+    marginLeft: 12,
+    fontWeight: "500",
   },
 });
